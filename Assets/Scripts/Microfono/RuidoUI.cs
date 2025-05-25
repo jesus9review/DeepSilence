971900547551d143880767ecc2475f1b56541432
JesusSalcedo <jesus9review@gmail.com>
2025-05-25 15:15:45 -0500

    Primera subida de proyecto
    El proyecto principal de unity

---
+++ Assets/Scripts/Microfono/RuidoUI.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MicrofonoUI : MonoBehaviour
+{
+    public MicrofonoManager microfono;
+    public Slider barraRuido;
+    public TextMeshProUGUI textoPorcentaje;
+
+    void Update()
+    {
+        float ruido = microfono.ruidoTotal;
+        barraRuido.value = ruido;
+        textoPorcentaje.text = Mathf.RoundToInt(ruido * 100f) + "%";
+    }
+}
