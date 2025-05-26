4631d3694fee70784b23af0aca6697ed13b432f1
xkennethvalero <kjvsevilla@gmail.com>
2025-05-26 10:28:26 -0500

    Inventario adelanto
    configuración de inventario dinámico para que al guardar el objeto visualmente salga

---
+++ Assets/Scripts/Inventario/InformaciónObjetos.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InformaciónObjetos : MonoBehaviour
+{
+    public string nombreObjeto;
+    public string Descripcion;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Inventario/InventarioObjetos.cs
+++ Assets/Scripts/Inventario/InventarioObjetos.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventarioObjetos : MonoBehaviour
 {
@@ -9,7 +10,18 @@ public class InventarioObjetos : MonoBehaviour
     private NombresObjetos objetosDisponibles;
     private BaseDeDatosJugador objetosObtenidos;
 
+    private Transform panelObjetos;
+    private Transform panelNotas;
 
+    //IconosObjetos
+    public GameObject objetoInventario;
+    
+
+    void Awake()
+    {
+        panelObjetos = transform.GetChild(0);
+        panelNotas = transform.GetChild(1);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -21,40 +33,56 @@ public class InventarioObjetos : MonoBehaviour
         for (int i = 0; i < objetosDisponibles.ItemsString.Length; i++)
         {
             string item = objetosDisponibles.ItemsString[i];
-            if (item.Substring(0, 4) == "nota")
+
+
+            int j = 0;
+            while (j < objetosObtenidos.slotsBasicos.Length)
             {
-                int j = 0;
-                while (j < objetosObtenidos.slotsBasicos.Length)
+                if (item == objetosObtenidos.slotsBasicos[j])
                 {
-                    if (item == objetosObtenidos.slotsBasicos[j])
+                    GameObject objeto;
+                    Sprite icono;
+
+                    if (item.Substring(0, 4) == "nota")
                     {
-                        print("Tengo esta nota " + objetosObtenidos.slotsBasicos[j]);
-                        break;
+                        objeto = Instantiate(objetoInventario, panelNotas);
+                        icono = Resources.Load<Sprite>("IconosInventario/nota");
+                        
                     }
-                    j++;
+                    else
+                    {
+                        objeto = Instantiate(objetoInventario, panelObjetos);
+                        icono = Resources.Load<Sprite>("IconosInventario/" + item);
+                    }
+
+                    objeto.GetComponent<Image>().sprite = icono;
+
+                    break;
                 }
+                j++;
             }
+
         }
 
         //Objetos
-        for (int i = 0; i < objetosDisponibles.ItemsString.Length; i++)
-        {
-            string item = objetosDisponibles.ItemsString[i];
-            if (item.Substring(0, 4) != "nota")
-            {
-                int j = 0;
-                while (j < objetosObtenidos.slotsBasicos.Length)
-                {
-                    if (item == objetosObtenidos.slotsBasicos[j])
-                    {
-                        //Instantiate()
-                        print("Tengo este objeto " + objetosObtenidos.slotsBasicos[j]);
-                        break;
-                    }
-                    j++;
-                }
-            }     
-        }
+        /* for (int i = 0; i < objetosDisponibles.ItemsString.Length; i++)
+         {
+             string item = objetosDisponibles.ItemsString[i];
+             if (item.Substring(0, 4) != "nota")
+             {
+                 int j = 0;
+                 while (j < objetosObtenidos.slotsBasicos.Length)
+                 {
+                     if (item == objetosObtenidos.slotsBasicos[j])
+                     {
+                         //Instantiate()
+                         print("Tengo este objeto " + objetosObtenidos.slotsBasicos[j]);
+                         break;
+                     }
+                     j++;
+                 }
+             }     
+         }*/
     }
 
     // Update is called once per frame
